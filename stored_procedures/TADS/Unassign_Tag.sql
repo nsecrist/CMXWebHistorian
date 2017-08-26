ade36cd3a2161831b3d3414e62b9998479cdfe57
nsecrist <secrist24@gmail.com>
2017-08-26 14:26:35 -0500

    Added restful API for TADS. Wrote swagger documentation and added it to tas/api/v1/docs route

---
+++ stored_procedures/TADS/Unassign_Tag.sql
@@ -0,0 +1,26 @@
+USE [JCE]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [dbo].[Unassociate]
+GO
+
+CREATE PROCEDURE [dbo].[Unassociate](@mac VARCHAR(12), @date DateTime)
+AS BEGIN
+
+	UPDATE JCE_Assigned_Tags
+	SET DateUnassigned = @date
+	WHERE (DateUnassigned is NUll AND MAC_Address = @mac)
+
+
+	INSERT INTO JCE_Tag_MasterList
+	(MAC_Address, Tag_Status, StatusDate)
+	VALUES (@mac, 'Available', @date)
+
+END
+GO
