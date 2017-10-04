24bc862713e3592f2080287ce14f4f36b1bc7d1c
nsecrist <secrist24@gmail.com>
2017-10-04 15:18:47 -0500

    Modified notifications such that an array of notifications is handled in an asynchornous way.

--- routes/notification.js
+++ routes/notification.js
@@ -27,7 +27,7 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res) {
   var dt = new Date();
   var utcDate = dt.toUTCString();
-  console.log(utcDate + " -- POST to notification received.")
+  // console.log(utcDate + " -- POST to notification received.")
 
   // needed for the fs.appendfile debug output below
   // if (!fs.existsSync(dataDir)) {
@@ -35,72 +35,95 @@ router.post('/', function(req, res) {
   // }
 
   // Get the notification
-  var notification = req.body.notifications[0];
+  var notifications = req.body.notifications;
 
   // notification.jce_pid = "-1";
   // body = JSON.stringify(notification);
 
-  // Pass in the notification object, and return it with the added jce_pid
-  // then stringify the object and use it to call the stored procedure
-  body = JSON.stringify(pid.Lookup(notification));
+  processArray(notifications, function(notification) {
+    // Pass in the notification object, and return it with the added personnel info
+    // then stringify the object and use it to call the stored procedure
+    body = JSON.stringify(pid.Lookup(notification));
 
-  // json = body.substring(18, body.lastIndexOf('}')-1);
+    // json = body.substring(18, body.lastIndexOf('}')-1);
 
-  // debug output
-  // console.log(body);
+    // debug output
+    // console.log(body);
 
-  // more debug output
-  // fs.appendFile(path.join(dataDir, 'data.json'), body + '\n');
+    // more debug output
+    // fs.appendFile(path.join(dataDir, 'data.json'), body + '\n');
 
-  const pool = new sql.ConnectionPool(config, err => {
-    pool.request()
-      .input(parameter, sql.VarChar(8000), body)
-      .execute(sp, (err) => {
-        // ... error checks
-        if (err) {
-          console.log(err);
-          var dt = new Date();
-          var utcDate = dt.toUTCString();
-          result = utcDate + ' -- ' + sp + ' failed.'
-        }
-        else {
-          var dt = new Date();
-          var utcDate = dt.toUTCString();
-          result = utcDate + ' -- ' + sp + ' was successful.'
-        }
-        console.log(result);
-      });
-      if (err) {
-        console.log(err);
-      }
 
+    const pool = new sql.ConnectionPool(config, err => {
       pool.request()
         .input(parameter, sql.VarChar(8000), body)
-        .execute('Location_CVT_Insert', (err) => {
+        .execute(sp, (err) => {
           // ... error checks
           if (err) {
             console.log(err);
             var dt = new Date();
             var utcDate = dt.toUTCString();
-            result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' failed.'
+            result = utcDate + ' -- ' + sp + ' failed.'
           }
           else {
             var dt = new Date();
             var utcDate = dt.toUTCString();
-            result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' was successful.'
+            result = utcDate + ' -- ' + sp + ' was successful.'
           }
-          console.log(result);
+          // console.log(result);
         });
         if (err) {
           console.log(err);
         }
-  });
+
+        pool.request()
+          .input(parameter, sql.VarChar(8000), body)
+          .execute('Location_CVT_Insert', (err) => {
+            // ... error checks
+            if (err) {
+              console.log(err);
+              var dt = new Date();
+              var utcDate = dt.toUTCString();
+              result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' failed.'
+            }
+            else {
+              var dt = new Date();
+              var utcDate = dt.toUTCString();
+              result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' was successful.'
+            }
+            // console.log(result);
+          });
+          if (err) {
+            console.log(err);
+          }
+        });
   res.sendStatus(200);
+  })
 });
 
 /* GET notification page. */
 router.post('/battery_life', function(req, res, next) {
-  notification = req.body.notifications[0]
+  notification = req.body.notifications[0];
+
+  // needed for the fs.appendfile debug output below
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+  }
+
+  // more debug output
+  fs.appendFile(path.join(dataDir, 'battery_life.json'), notification + '\n');
+
 });
+
+function processArray(items, process) {
+  var todo = items.concat();
+
+  setTimeout(function() {
+    process(todo.shift());
+    if (todo.length > 0) {
+      setTimeout(arguments.callee, 25);
+    }
+  }, 25);
+}
 
 module.exports = router;
