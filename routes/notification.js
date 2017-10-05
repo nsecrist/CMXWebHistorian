05757af2118c38032b064f6e10ea8101a4188ff6
nsecrist <secrist24@gmail.com>
2017-10-05 11:54:17 -0500

    Fixed an issue with the SQL Connection pool causing duplicates of notificaitons to be added to the database. This also resolved issues related to the heap exponentially growing. /notification now responds qithin 1.33ms on average. Added an application metrics dashboard.

--- routes/notification.js
+++ routes/notification.js
@@ -6,17 +6,18 @@ var sql = require('mssql/msnodesqlv8');
 var db = require('../src/db.js');
 var pid = require('../src/pid_lookup.js');
 
-// var config = {
-//   driver: 'msnodesqlv8',
-//   connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost};Database={SecristTestDB};Trusted_Connection={yes};'
-// }
-
 var config = db.tads();
 
 var dataDir = './public/data/'
 
 const parameter = 'Location_NotificationJson';
 const sp = 'Location_NotificationInsertJson';
+
+const pool = new sql.ConnectionPool(config, err => {
+  if (err) {
+    console.log('Failed to open a SQL Database connection.', err.stack);
+  }
+});
 
 /* GET notification page. */
 router.get('/', function(req, res, next) {
@@ -25,8 +26,8 @@ router.get('/', function(req, res, next) {
 
 /* POST Notification from CMX */
 router.post('/', function(req, res) {
-  var dt = new Date();
-  var utcDate = dt.toUTCString();
+  // var dt = new Date();
+  // var utcDate = dt.toUTCString();
   // console.log(utcDate + " -- POST to notification received.")
 
   // needed for the fs.appendfile debug output below
@@ -35,95 +36,64 @@ router.post('/', function(req, res) {
   // }
 
   // Get the notification
-  var notifications = req.body.notifications;
+  var notification = req.body.notifications[0];
 
   // notification.jce_pid = "-1";
   // body = JSON.stringify(notification);
 
-  processArray(notifications, function(notification) {
-    // Pass in the notification object, and return it with the added personnel info
-    // then stringify the object and use it to call the stored procedure
-    body = JSON.stringify(pid.Lookup(notification));
+  // Pass in the notification object, and return it with the added jce_pid
+  // then stringify the object and use it to call the stored procedure
+  body = JSON.stringify(pid.Lookup(notification));
 
-    // json = body.substring(18, body.lastIndexOf('}')-1);
+  // json = body.substring(18, body.lastIndexOf('}')-1);
 
-    // debug output
-    // console.log(body);
+  // debug output
+  // console.log(body);
 
-    // more debug output
-    // fs.appendFile(path.join(dataDir, 'data.json'), body + '\n');
+  // more debug output
+  // fs.appendFile(path.join(dataDir, 'data.json'), body + '\n');
 
+  pool.request()
+    .input(parameter, sql.VarChar(8000), body)
+    .execute(sp, (err) => {
+      // ... error checks
+      if (err) {
+        console.log(err);
+        var dt = new Date();
+        var utcDate = dt.toUTCString();
+        result = utcDate + ' -- ' + sp + ' failed.'
+      }
+      else {
+        var dt = new Date();
+        var utcDate = dt.toUTCString();
+        result = utcDate + ' -- ' + sp + ' was successful.'
+      }
+      // console.log(result);
+    });
 
-    const pool = new sql.ConnectionPool(config, err => {
-      pool.request()
-        .input(parameter, sql.VarChar(8000), body)
-        .execute(sp, (err) => {
-          // ... error checks
-          if (err) {
-            console.log(err);
-            var dt = new Date();
-            var utcDate = dt.toUTCString();
-            result = utcDate + ' -- ' + sp + ' failed.'
-          }
-          else {
-            var dt = new Date();
-            var utcDate = dt.toUTCString();
-            result = utcDate + ' -- ' + sp + ' was successful.'
-          }
-          // console.log(result);
-        });
+    pool.request()
+      .input(parameter, sql.VarChar(8000), body)
+      .execute('Location_CVT_Insert', (err) => {
+        // ... error checks
         if (err) {
           console.log(err);
+          var dt = new Date();
+          var utcDate = dt.toUTCString();
+          result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' failed.'
         }
-
-        pool.request()
-          .input(parameter, sql.VarChar(8000), body)
-          .execute('Location_CVT_Insert', (err) => {
-            // ... error checks
-            if (err) {
-              console.log(err);
-              var dt = new Date();
-              var utcDate = dt.toUTCString();
-              result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' failed.'
-            }
-            else {
-              var dt = new Date();
-              var utcDate = dt.toUTCString();
-              result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' was successful.'
-            }
-            // console.log(result);
-          });
-          if (err) {
-            console.log(err);
-          }
-        });
+        else {
+          var dt = new Date();
+          var utcDate = dt.toUTCString();
+          result = utcDate + ' -- ' + 'Location_CVT_Insert' + ' was successful.'
+        }
+        // console.log(result);
+      });
   res.sendStatus(200);
-  })
 });
 
 /* GET notification page. */
 router.post('/battery_life', function(req, res, next) {
-  notification = req.body.notifications[0];
-
-  // needed for the fs.appendfile debug output below
-  if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir);
-  }
-
-  // more debug output
-  fs.appendFile(path.join(dataDir, 'battery_life.json'), notification + '\n');
-
+  notification = req.body.notifications[0]
 });
-
-function processArray(items, process) {
-  var todo = items.concat();
-
-  setTimeout(function() {
-    process(todo.shift());
-    if (todo.length > 0) {
-      setTimeout(arguments.callee, 25);
-    }
-  }, 25);
-}
 
 module.exports = router;
