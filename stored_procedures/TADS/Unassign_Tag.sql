bcb8cf8605fe8b08ead8d7c9548f358b3bb31bd8
nsecrist <secrist24@gmail.com>
2017-10-12 14:35:33 -0500

    Modified scripts and created a single init DB script

--- stored_procedures/TADS/Unassign_Tag.sql
+++ stored_procedures/TADS/Unassign_Tag.sql
@@ -1,23 +1,16 @@
-/*    ==Scripting Parameters==
-
-    Source Server Version : SQL Server 2016 (13.0.1742)
-    Source Database Engine Edition : Microsoft SQL Server Standard Edition
-    Source Database Engine Type : Standalone SQL Server
-
-    Target Server Version : SQL Server 2017
-    Target Database Engine Edition : Microsoft SQL Server Standard Edition
-    Target Database Engine Type : Standalone SQL Server
-*/
-
 USE [JCE]
 GO
-/****** Object:  StoredProcedure [dbo].[Unassociate]    Script Date: 10/12/2017 1:46:07 PM ******/
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[Unassociate](@mac VARCHAR(12), @date DateTime)
+DROP PROCEDURE IF EXISTS [dbo].[Unassociate]
+GO
+
+CREATE PROCEDURE [dbo].[Unassociate](@mac VARCHAR(12), @date DateTime)
 AS BEGIN
 
 	UPDATE JCE_Assigned_Tags
