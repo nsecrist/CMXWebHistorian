8789fa9ab4bd766d7c09d387e4bb12457928cfdc
nsecrist <secrist24@gmail.com>
2017-10-12 13:50:11 -0500

    Updated the associate and unassociate stored procedures. Changed view used for hash table.

--- stored_procedures/TADS/Unassign_Tag.sql
+++ stored_procedures/TADS/Unassign_Tag.sql
@@ -1,16 +1,23 @@
+/*    ==Scripting Parameters==
+
+    Source Server Version : SQL Server 2016 (13.0.1742)
+    Source Database Engine Edition : Microsoft SQL Server Standard Edition
+    Source Database Engine Type : Standalone SQL Server
+
+    Target Server Version : SQL Server 2017
+    Target Database Engine Edition : Microsoft SQL Server Standard Edition
+    Target Database Engine Type : Standalone SQL Server
+*/
+
 USE [JCE]
 GO
-
+/****** Object:  StoredProcedure [dbo].[Unassociate]    Script Date: 10/12/2017 1:46:07 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
-DROP PROCEDURE IF EXISTS [dbo].[Unassociate]
-GO
-
-CREATE PROCEDURE [dbo].[Unassociate](@mac VARCHAR(12), @date DateTime)
+ALTER PROCEDURE [dbo].[Unassociate](@mac VARCHAR(12), @date DateTime)
 AS BEGIN
 
 	UPDATE JCE_Assigned_Tags
@@ -20,7 +27,6 @@ AS BEGIN
 
 	INSERT INTO JCE_Tag_MasterList
 	(MAC_Address, Tag_Status, StatusDate)
-	VALUES (@mac, 'Available', @date)
+	VALUES (@mac, 'AVAILABLE', @date)
 
 END
-GO
