c55b4a56c5480a53e1a95d71abd90466c902aff5
nsecrist <secrist24@gmail.com>
2018-03-12 07:25:50 -0500

    Updated scripts to remove time column and add a smalldatetime column for indexing

--- stored_procedures/Legacy Data Correction.sql
+++ stored_procedures/Legacy Data Correction.sql
@@ -3,7 +3,8 @@ DECLARE @cCurrentServerTime nchar(28)
 
 DECLARE @validDateTime datetimeoffset(7)
 DECLARE @validDate	date
-DECLARE @validTime time
+DECLARE @indexTimestamp smalldatetime
+DECLARE @validTimestamp datetime
 
 DECLARE MY_CURSOR CURSOR 
 FOR 
@@ -16,10 +17,11 @@ WHILE @@FETCH_STATUS = 0
 BEGIN
 	set @validDateTime = convert(datetimeoffset(7), left(@cCurrentServerTime,len(@cCurrentServerTime) -2) + ':' + right(@cCurrentServerTime,2), 4)
 	set @validDate = cast(@validDateTime as date)
-	set @validTime = cast(@validDateTime as time)
+	set @validTimestamp = cast(@validDateTime as datetime)
+	set @indexTimestamp = cast(@validDateTime as smalldatetime)
 
     UPDATE Location_History
-	SET validDateTime = @validDateTime, validDate = @validDate, validTime = @validTime
+	SET validDateTime = @validDateTime, validDate = @validDate, validTimestamp = @validTimestamp, indexTimestamp = @indexTimestamp
 	WHERE macAddress = @cMacAddress AND currentServerTime = @cCurrentServerTime
     PRINT @cMacAddress + ' : ' + @cCurrentServerTime
     FETCH NEXT FROM MY_CURSOR INTO @cMacAddress, @cCurrentServerTime
--- stored_procedures/StringDateConvert.sql
+++ stored_procedures/StringDateConvert.sql
@@ -1,26 +1,27 @@
 INSERT INTO Location_History
-SELECT *
-        ,cast(convert(datetimeoffset(7),t2.validDateTime,4) as date) validDate
-		,cast(convert(datetimeoffset(7),t2.validDateTime,4) as time) validTime
-FROM (
-SELECT *
-        ,convert(datetimeoffset(7), left(t1.currentServerTime,len(t1.currentServerTime) -2) + ':' + right(t1.currentServerTime,2), 4) validDateTime
-FROM (
-    SELECT *
-    FROM OPENJSON(@Location_History_Json)
-		WITH (  
-			macAddress nchar(17) 'strict $.macAddress'
-    	   ,mapHierarchyString nvarchar(256) 'lax $.mapInfo.mapHierarchyString'
-    	   ,mapCoordinateX float 'lax $.mapCoordinate.x'
-    	   ,mapCoordinateY float 'lax $.mapCoordinate.y'
-    	   ,mapCoordinateUnit nvarchar(32) 'lax $.mapCoordinate.unit'
-    	   ,confidenceFactor int 'lax $.confidenceFactor'
-    	   ,currentServerTime nchar(28) 'lax $.statistics.currentServerTime'
-    	   ,firstLocatedTime nchar(28) 'lax $.statistics.firstLocatedTime'
-    	   ,lastLocatedTime nchar(28) 'lax $.statistics.lastLocatedTime'
-    	   ,batteryPercentRemaining int 'lax $.batteryInfo.percentRemaining'
-    	   ,batteryDaysRemaining int 'lax $.batteryInfo.daysRemaining'
-		   ,jcePid int 'strict $.jce_pid'
-            )
+	SELECT *
+        	,cast(convert(datetimeoffset(7),t2.validDateTime,4) as date) validDate
+		,cast(convert(datetimeoffset(7),t2.validDateTime,4) as datetime) validTimestamp
+		,cast(convert(datetimeoffset(7),t2.validDateTime,4) as smalldatetime) indexTimestamp
+	FROM (
+		SELECT *
+			,convert(datetimeoffset(7), left(t1.currentServerTime,len(t1.currentServerTime) -2) + ':' + right(t1.currentServerTime,2), 4) validDateTime
+		FROM (
+			SELECT *
+			FROM OPENJSON(@Location_History_Json)
+				WITH (  
+					macAddress nchar(17) 'strict $.macAddress'
+    					,mapHierarchyString nvarchar(256) 'lax $.mapInfo.mapHierarchyString'
+    					,mapCoordinateX float 'lax $.mapCoordinate.x'
+    					,mapCoordinateY float 'lax $.mapCoordinate.y'
+    					,mapCoordinateUnit nvarchar(32) 'lax $.mapCoordinate.unit'
+    					,confidenceFactor int 'lax $.confidenceFactor'
+    					,currentServerTime nchar(28) 'lax $.statistics.currentServerTime'
+    					,firstLocatedTime nchar(28) 'lax $.statistics.firstLocatedTime'
+    					,lastLocatedTime nchar(28) 'lax $.statistics.lastLocatedTime'
+    					,batteryPercentRemaining int 'lax $.batteryInfo.percentRemaining'
+    					,batteryDaysRemaining int 'lax $.batteryInfo.daysRemaining'
+					,jcePid int 'strict $.jcePid'
+				)
 		) as t1
-) as t2
+	) as t2
